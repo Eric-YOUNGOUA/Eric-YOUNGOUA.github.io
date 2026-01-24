4f70c51226264f0e598674deb74f1e2f8649ff5b
ERIC YOUNGOUA <ericyoungoua@gmail.com>
2026-01-24 17:59:50 +0100

    Mise à jour des compétences fin semestre 1

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Optionnel : Ajouter une ombre à la barre de navigation au défilement
+//Ajouter une ombre à la barre de navigation au défilement
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 50) {
